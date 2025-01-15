9c2c1e6703999b34d8028f23abbdf08178a78979
Uniba-Muzammil <unibamuzammil8@gmail.com>
2025-01-15 21:42:17 +0500

    update

--- src/components/MuiTextField.jsx
+++ src/components/MuiTextField.jsx
@@ -5,7 +5,7 @@ export default function MuiTextField() {
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction='row'>
-        <TextField/>
+        <TextField label='name'/>
 
       </Stack>
     </Stack>
