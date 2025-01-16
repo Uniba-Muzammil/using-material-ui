e7bc4c964179016bfb037867f919869921e05c43
Uniba-Muzammil <unibamuzammil8@gmail.com>
2025-01-16 12:23:40 +0500

    update

--- src/components/MuiTextField.jsx
+++ src/components/MuiTextField.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Stack, TextField} from '@mui/material'
+import { useState } from 'react';
 
 export default function MuiTextField() {
+  const [value, setValue] = useState("");
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction='row'>
@@ -11,7 +13,22 @@ export default function MuiTextField() {
       </Stack>
       <Stack spacing={2} direction='row'>
       <TextField label='Small secondary' size='small' color='secondary'/>
-
+      </Stack>
+      <Stack spacing={2} direction='row'>
+      <TextField label='Form Input'  
+      required
+      value={value}
+      onChange={(e)=>setValue(e.target.value)}
+      error={!value}
+      helperText={
+      !value ? 'Required' : 'Do not share your password with anyone'
+      }/>
+      <TextField
+      label="Password"
+      type='password'
+      helperText="Do not share your password with anyone"
+      
+      />
       </Stack>
     </Stack>
   );
